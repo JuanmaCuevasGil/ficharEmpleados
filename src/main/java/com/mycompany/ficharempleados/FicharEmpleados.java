17aa4096a88bffacb0de2267d9915cb3f3779b8b
JuanmaCuevasGil <juan.cuevas.gil@colexio-karbo.com>
2024-01-16 19:49:53 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/ficharempleados/FicharEmpleados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ficharempleados;
+
+/**
+ *
+ * @author Juanma
+ */
+public class FicharEmpleados {
+
+    public static void main(String[] args) {
+        new Frame().setVisible(true);
+    }
+}
